eecb2f6780f4c1a92adf7b0f6d3c0c8a22038926
unknown <kevinkarmacharya@yahoo.com>
2022-07-11 18:04:11 -0700

    added more projects and fixed styling

--- app.js
+++ app.js
@@ -14,32 +14,29 @@ $(document).ready(function() {
 
 $(document).scroll(function() {
     var y = $(this).scrollTop();
-    if (y > 200) {
+    if (y > 250) {
       $("#about-me-section").removeClass("temp-hide");
       $('#about-me-section').fadeIn();
       // $(".about-me-section").css('visibility', 'visible');
     }
 
     if(y > 1000){
-      $("#work-experience-section").removeClass("temp-hide")
+      $("#work-experience-section").removeClass("temp-hide");
       $('#work-experience-section').fadeIn();
     }
 
-    if(y > 1800){
-      $("#projects-section").removeClass("temp-hide")
+    if(y > 1650){
+      $("#projects-section").removeClass("temp-hide");
       $('#projects-section').fadeIn();
     }
-    if(y > 2600){
-      $("#skills-section").removeClass("temp-hide")
+    if(y > 2750){
+      $("#skills-section").removeClass("temp-hide");
       $('#skills-section').fadeIn();
     }
-    if(y > 3000){
-      $("#contact-section").removeClass("temp-hide")
+    if(y > 3400){
+      $("#contact-section").removeClass("temp-hide");
+      $("#footer-section").removeClass("temp-hide");
       $('#contact-section').fadeIn();
-    }
-    if(y > 3200){
-      $("#footer-section").removeClass("temp-hide")
       $('#footer-section').fadeIn();
     }
-
   });
